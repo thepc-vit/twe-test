e065bae733c60a2e206592855d2d13dd4ae34897
thepc-vit <62247526+thepc-vit@users.noreply.github.com>
2020-07-08 18:39:35 +0530

    Add files via upload

---
+++ precache-manifest.ce69b2acea9589884d6e8211d9ec6459.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1397458827c898bcfb82990c0c23e763",
+    "url": "/tweapp/index.html"
+  },
+  {
+    "revision": "43deef503efedcd097fd",
+    "url": "/tweapp/static/css/2.13c6718b.chunk.css"
+  },
+  {
+    "revision": "bff74fd995922e1a1891",
+    "url": "/tweapp/static/css/main.00770f9f.chunk.css"
+  },
+  {
+    "revision": "43deef503efedcd097fd",
+    "url": "/tweapp/static/js/2.c8dcbecb.chunk.js"
+  },
+  {
+    "revision": "8e5f0863f7f89b08e73021ac11384df6",
+    "url": "/tweapp/static/js/2.c8dcbecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff74fd995922e1a1891",
+    "url": "/tweapp/static/js/main.0eca3aed.chunk.js"
+  },
+  {
+    "revision": "cc8efdf0c032f567688b",
+    "url": "/tweapp/static/js/runtime-main.b4eb4a23.js"
+  },
+  {
+    "revision": "5727f869509c87f4d25c2225f0968573",
+    "url": "/tweapp/static/media/chevron-left.5727f869.svg"
+  },
+  {
+    "revision": "9c59704b1de34c9fbb54c100b907ac85",
+    "url": "/tweapp/static/media/chevron-right.9c59704b.svg"
+  },
+  {
+    "revision": "cbeab2705cd1a8c5d774eac8ed9c9619",
+    "url": "/tweapp/static/media/dollar-sign.cbeab270.svg"
+  },
+  {
+    "revision": "1c251c8ff2222207650b4f24a2159cbb",
+    "url": "/tweapp/static/media/dot-pattern.1c251c8f.svg"
+  },
+  {
+    "revision": "7064f155dc1611550f5ade904f1964e3",
+    "url": "/tweapp/static/media/email-newsletter-icon.7064f155.svg"
+  },
+  {
+    "revision": "5e030c93ee64514b9fcdc887289ce8a9",
+    "url": "/tweapp/static/media/facebook-icon.5e030c93.svg"
+  },
+  {
+    "revision": "f9776f55173cd40b2b3f3fed1c7443c4",
+    "url": "/tweapp/static/media/logo.f9776f55.svg"
+  },
+  {
+    "revision": "b2f804bda31dee415fc2b1ff7ba9315f",
+    "url": "/tweapp/static/media/map-pin.b2f804bd.svg"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/tweapp/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "36f0e6ee645c508fdc115426b87baba6",
+    "url": "/tweapp/static/media/play-circle.36f0e6ee.svg"
+  },
+  {
+    "revision": "ac8ea2135e1379656f02ca2eea26cc87",
+    "url": "/tweapp/static/media/star.ac8ea213.svg"
+  },
+  {
+    "revision": "a200bad45b923b0b206917b16755f83f",
+    "url": "/tweapp/static/media/stats-illustration.a200bad4.svg"
+  },
+  {
+    "revision": "ec52da39f670db5b4b3c32f54914a919",
+    "url": "/tweapp/static/media/svg-decorator-blob-1.ec52da39.svg"
+  },
+  {
+    "revision": "159be2476eedd8b63bb0c7e0a58d0e47",
+    "url": "/tweapp/static/media/svg-decorator-blob-3.159be247.svg"
+  },
+  {
+    "revision": "7189052ca672d113f057d9a4f3cb5efe",
+    "url": "/tweapp/static/media/twitter-icon.7189052c.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/tweapp/static/media/x.b0200bf7.svg"
+  },
+  {
+    "revision": "45e500e6c6c6577b063b28adc39fb422",
+    "url": "/tweapp/static/media/youtube-icon.45e500e6.svg"
+  }
+]);
